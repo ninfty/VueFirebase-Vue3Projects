a515b68573fa0f12307ef240f948e18041bbff04
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-12-25 17:55:06 -0300

    Creating a Filter Nav

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="current = $event" :current="current" />
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
@@ -10,13 +11,15 @@
 
 <script>
 import SingleProject from '../components/SingleProject'
+import FilterNav from '../components/FilterNav'
 
 export default {
   name: 'Home',
-  components: { SingleProject },
+  components: { SingleProject, FilterNav },
   data() {
     return {
-      projects: []
+      projects: [],
+      current: 'all'
     };
   },
   mounted() {
