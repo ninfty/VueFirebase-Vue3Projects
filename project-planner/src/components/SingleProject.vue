321677ec84c63fda3d31a40bc2af3e9a64736ea8
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-12-25 14:45:56 -0300

    Single Project Template

---
+++ project-planner/src/components/SingleProject.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="toggleDetails()">{{ project.title }}</h3>
+    </div>
+    <div v-if="showDetails" class="details">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['project'],
+  data() {
+    return {
+      showDetails: false
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.showDetails = !this.showDetails
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
+    border-left: 4px solid #e90074;
+  }
+  h3 {
+    cursor: pointer;
+  }
+</style>
