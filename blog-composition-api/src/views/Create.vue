cac7fb8b47cfa88782fa51364d86e5951c87f78e
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-12-26 10:59:11 -0300

    Making a POST Request

--- blog-composition-api/src/views/Create.vue
+++ blog-composition-api/src/views/Create.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="create">
-    <form>
+    <form @submit.prevent="handleSubmit">
       <label>Title:</label>
       <input v-model="title" type="text" required>
       <label>Content:</label>
@@ -33,7 +33,21 @@ export default {
       tag.value = ''
     }
 
-    return { body, title, tags, tag, handleKeydown }
+    const handleSubmit = async () => {
+      const post = {
+        title: title.value,
+        body: body.value,
+        tags: tags.value
+      }
+
+      await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(post)
+      })
+    }
+
+    return { body, title, tags, tag, handleKeydown, handleSubmit }
   },
 }
 </script>
