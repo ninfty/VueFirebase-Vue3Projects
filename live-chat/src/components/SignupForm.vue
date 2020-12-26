760d896bd3e92871c1abf138ba9acc8e185bbe7c
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-12-26 18:49:00 -0300

    Setup Context & Emitting Events

--- live-chat/src/components/SignupForm.vue
+++ live-chat/src/components/SignupForm.vue
@@ -13,7 +13,7 @@ import { ref } from 'vue'
 import useSignup from '../composables/useSignup'
 
 export default {
-  setup() {
+  setup(props, context) {
     const { error, signup } = useSignup()
 
     // refs
@@ -23,7 +23,9 @@ export default {
 
     const handleSubmit = async () => {
       await signup(email.value, password.value, displayName.value)
-      console.log('user signed up')
+      if (!error.value) {
+        context.emit('signup')
+      }
     }
 
     return { displayName, email, password, handleSubmit, error }
