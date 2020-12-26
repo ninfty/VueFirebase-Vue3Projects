942bc651f2b86221718ce21086e030d00a4f3e43
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-12-26 17:07:46 -0300

    Firebase Authentication Setup

--- live-chat/src/firebase/config.js
+++ live-chat/src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCgC3Jvgv1Kyk9_1SY_bbHaLPfIuzkBI3E",
@@ -13,7 +14,8 @@ const firebaseConfig = {
 // init firebase
 firebase.initializeApp(firebaseConfig)
 
+const projectAuth = firebase.auth()
 const projectFirestore = firebase.firestore()
 const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export { projectFirestore, timestamp }
+export { projectAuth, projectFirestore, timestamp }
