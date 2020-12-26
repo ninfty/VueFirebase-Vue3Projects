7e3aca74b50abbec09e20085f0c727e8dbbfbd0e
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-12-25 22:00:34 -0300

    Template Refs in the Setup

--- blog-composition-api/src/views/Home.vue
+++ blog-composition-api/src/views/Home.vue
@@ -1,28 +1,31 @@
 <template>
   <div class="home">
-    home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click me</button>
+    <h1>Home</h1>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
-
     let name = 'mario'
     let age = 30
 
-    const handleClick = () => {
-      console.log('you clicked me')
+    const p = ref(null)
+
+    const handleClick = (e) => {
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello, ninjas'
     }
 
-    return { name, age, handleClick }
+    console.log(p.value)
+    
+    return { name, age, handleClick, p }
   },
-  mounted() {
-    console.log('mounted')
-  }
 }
 </script>
