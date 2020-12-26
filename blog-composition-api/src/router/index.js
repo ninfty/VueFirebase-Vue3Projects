ee8795a0d1c514d1ebbcebf334a667a552138712
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-12-26 10:53:59 -0300

    Making a Web Form

--- blog-composition-api/src/router/index.js
+++ blog-composition-api/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home'
 import Details from '../views/Details'
+import Create from '../views/Create'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'Details',
     component: Details,
     props: true
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create
   }
 ]
 
